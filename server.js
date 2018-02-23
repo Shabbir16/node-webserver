d6bc76b5bd868b3a133eff8f4642adbad7ad706e
shabbir <pshabbir36@gmail.com>
2018-02-24 00:23:28 +0530

    Port changed

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs= require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000 ;
 var app = express();
 
 hbs.registerPartials(__dirname+'/views/partials');
@@ -61,7 +62,7 @@ app.get('/bad',(req,res)=>{
     });
 }); 
 
-app.listen(3000);
+app.listen(port);
 
 //This is to test commit
 //Hi i am shabbir
